fff1ce0ec428278642d2289396db1ce41ec7a9b3
dylanls <dylanaislinn@gmail.com>
2017-05-31 00:30:35 -0400

    Update config.php
    removed curse words

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
    define('DB_DATABASE', 'SOCIAL');
    $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
    if(mysqli_connect_errno()) {
-	echo "SQL CONNECTION FAILED YOU COCK" . mysqli_connect_error();	
+	echo "SQL CONNECTION FAILED" . mysqli_connect_error();	
 	exit();	
 	}
 	
