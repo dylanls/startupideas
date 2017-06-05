3859aede5cff3d158764fb85ce1733c6f7d5fefd
zai666 <hallinson6@outlook.com>
2017-06-05 11:34:36 -0400

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,6 +1,7 @@
 <?php
   session_start();
   include("config.php");
+  if(!isset($_SESSION['login_username']) || !isset($_SESSION['login_userid'])) { exit();
   if($_SERVER["REQUEST_METHOD"] == "POST"){
     $userID = $_SESSION['login_userid'];
     $submittedIndustry = mysqli_real_escape_string($db, $_POST['industry']);
